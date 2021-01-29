0071580f400493a0a35e698be4803674d6c72aa8
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2021-01-30 00:11:33 +0900

    now

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :devices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  put "devices/enable"
-  put "devices/disable"
+  get "devices/enable"
+  get "devices/disable"
 end
